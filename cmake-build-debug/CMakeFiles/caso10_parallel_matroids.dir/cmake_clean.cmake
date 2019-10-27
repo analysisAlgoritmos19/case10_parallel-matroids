a9519c834a91e1acb4a01e86bbf3890b94acbc21
Esteban Salas <esalasestrada@gmail.com>
2019-10-27 13:31:16 -0600

    initial commit

---
+++ cmake-build-debug/CMakeFiles/caso10_parallel_matroids.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caso10_parallel_matroids.dir/main.c.o"
+  "caso10_parallel_matroids"
+  "caso10_parallel_matroids.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/caso10_parallel_matroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/caso10_parallel_matroids.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for caso10_parallel_matroids.
+# This may be replaced when dependencies are built.
