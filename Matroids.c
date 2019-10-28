50f74ae8265d7556e4832fdb832688c13ce0928c
Esteban Salas <esalasestrada@gmail.com>
2019-10-28 16:40:36 -0600

    Pruebas para el puntero *void

--- Matroids.c
+++
@@ -1,9 +0,0 @@
-
-#include "Matroids.h"
-
-struct Matroid{
-
-    void *S;
-    void *I;
-    void *w;
-};
--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include "Matroids.h"
+
+struct Matroid {
+
+    void *S;
+    void *I;
+    void *w;
+};
 
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+    void **ptr; // Declaring the pointer variable 'ptr' of data type int
+
+    char a = 'b'; // Declaring a normal variable 'a' and assigning value 10 to it.
+
+    printf("%d", a);
+
+    //ptr= (void**) &a; // Assigning address of variable 'a' to pointer variable 'ptr'
+
+    //printf("The value inside pointer is= %c",*ptr); // See the value is printed using *ptr;
+
 }
