cba8b4119d2f076737752ac780ff323fe75ec6de
Esteban Salas <esalasestrada@gmail.com>
2019-10-28 20:19:36 -0600

    Casi lista la parte a, solo falta de implementar que le *void se iguale a una variable y revisar el correcto funcionamiento de OpenMP

--- main.c
+++ main.c
@@ -7,17 +7,78 @@ struct Matroid {
     void *w;
 };
 
+void evaluate_matroids (struct Matroid matroids_list[], int size_of_s){
+
+    int size_of_matroids_list = sizeof(matroids_list) / sizeof(matroids_list[0]);
+
+    #pragma omp parallel for
+
+    for (int matroid_index = 0; matroid_index < size_of_matroids_list; ++matroid_index) {
+
+        struct Matroid matroid_element;
+
+        matroid_element = *(matroids_list + matroid_index);
+
+        #pragma omp parallel for
+
+        for (int element_in_s_index = 0; element_in_s_index < size_of_s ; ++element_in_s_index) {
+
+            void * element_in_s = matroid_element.S + element_in_s_index;
+
+        }
+
+
+
+    }
+
+}
+
 
 int main() {
 
-    void **ptr; // Declaring the pointer variable 'ptr' of data type int
+    //void **ptr; // Declaring the pointer variable 'ptr' of data type int
 
-    char a = 'b'; // Declaring a normal variable 'a' and assigning value 10 to it.
+    //int balance[5] = {100, 2, 3, 7, 150};
 
-    printf("%d", a);
+    //testMatroid.S = (void **) balance;
 
-    //ptr= (void**) &a; // Assigning address of variable 'a' to pointer variable 'ptr'
+    //int *test = (int *) testMatroid.S;
 
-    //printf("The value inside pointer is= %c",*ptr); // See the value is printed using *ptr;
+    //ptr= (void**) &balance[0]; // Assigning address of variable 'a' to pointer variable 'ptr'
+
+    //printf("The value inside pointer is= %d",*((int *)testMatroid.S+3)); // See the value is printed using *ptr;
+
+
+    struct Matroid test_array[3];
+
+    struct Matroid test1,test2,test3;
+
+    int array1 [] = {1,2,3};
+    int array2 [] = {4,5,6};
+    int array3 [] = {7,8,9};
+
+    test1.S = array1;
+    test2.S = array2;
+    test3.S = array3;
+
+    test_array[0] = test1;
+    test_array[1] = test2;
+    test_array[2] = test3;
+
+    //printf("%d", sizeof(test_array));
+
+    for (int i = 0; i < 3; ++i) {
+
+        struct Matroid matroid_element;
+
+        matroid_element = *(test_array + i);
+
+        void * element = matroid_element.S; //lib hasta aqui.
+
+        int * newelement = (int * )element + i;
+
+        printf("%d\n",*newelement);
+
+    }
 
 }
