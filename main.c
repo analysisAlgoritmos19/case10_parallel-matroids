81a8c2d6eb67869b7d0d3f23d9c34dd221c3d7db
LuisDavidJv99 <luisdavidjv86@estudiantec.cr>
2019-10-29 22:33:15 -0600

    Se creo una funcion para recorrer la lista de matroids, ademas se pulio el metodo de evaluar matroid para asignar los elementos de s que funciona a el I del matroid. Sin embargo todavia se deben pulir problemas de punteros y agregar la libreria openmp al trabajo para que se realize en paralelo

--- main.c
+++ main.c
@@ -6,13 +6,14 @@ struct Matroid {
     void *S;
     void *I;
     void* (*function) (void *);
+    int type_size;
 };
 
 bool is_pair(void* number_to_check){
 
     int true_number_to_check = *(int*) number_to_check;
 
-    printf("este es el int: %d \n", true_number_to_check);
+    //printf("este es el int: %d \n", true_number_to_check);
 
     if(true_number_to_check % 2 == 0)
         return true;
@@ -21,57 +22,75 @@ bool is_pair(void* number_to_check){
 }
 
 
-int evaluate_matroid (struct Matroid matroid, int size_of_s){
+int evaluate_matroid(struct Matroid* matroid, int size_of_s){
 
-    printf("El valor de matroid S es: %d\n",matroid.S);
+    //printf("El valor de matroid  es: %d\n",matroid);
 
     int counter = 0;
 
     void *arrayI [size_of_s];
 
-    void* element_in_s = matroid.S;
+    void* element_in_s = matroid->S;
 
     int num = *(int *)element_in_s;
 
-    printf("El valor de num es: %d\n",num);
+    //printf("El valor de num es: %d\n",num);
 
 
     for (int element_in_s_index = 0; element_in_s_index < size_of_s ; ++element_in_s_index) {
 
-        if (matroid.function(element_in_s)){
+        if (matroid->function(element_in_s)){
             arrayI[counter]  = element_in_s;
+            printf("El valor del elemento es %d \n", *(int*) arrayI[counter]);
             counter++;
         }
-        element_in_s = element_in_s+4;
+        element_in_s = element_in_s+matroid->type_size;
     }
-    matroid.I = arrayI;
-    int result =  *(int *)matroid.I + 4;
-    printf("El valor de matroid I es: %d\n", matroid.I);
-    printf("El valor de result es: %d \n", result);
+    printf("El valor de arrayI es %d \n", arrayI);
+
+    printf("El valor de arrayI es %d \n", arrayI[0]);
+
+    printf("El valor de arrayI es %d \n", *(arrayI));
+    matroid->I = arrayI[0];
+    int result =  *(int *)matroid->I;
+
+    //printf("El valor de matroid es%d \n", matroid);
     return counter;
 }
 
+void evaluate_array_matroid(struct Matroid* ptr_array_matroid, int sizeArray, int size_of_s){
 
+    {
+        for (int matroid_index = 0; matroid_index < sizeArray; matroid_index++) {
+
+            evaluate_matroid(ptr_array_matroid, size_of_s);
+
+            ptr_array_matroid++;
+        }
+}
 
 
 int main() {
-    //struct Matroid test_array[3];
-
+    struct Matroid test_array[3];
 
     struct Matroid test1, test2, test3;
 
     int array1[] = {1, 2, 3};
     int array2[] = {4, 5, 6};
-    //int array3 [] = {7,8,9};
+    int array3 [] = {7,8,9};
 
 
     test1.S = array1;
     test2.S = array2;
-    //test3.S = array3;
+    test3.S = array3;
 
-    int arrayI2 = {0, 0, 0};
+    test1.type_size = 4;
+    test2.type_size = 4;
+    test3.type_size = 4;
 
-    test1.I = arrayI2;
+    //int arrayI2 = {0, 0, 0};
+
+    //test1.I = arrayI2;
 
     //test_array[0] = test1;
     //test_array[1] = test2;
@@ -81,11 +100,21 @@ int main() {
 
     test2.function = is_pair;
 
+    test3.function = is_pair;
+
+    test_array[0] = test1;
+    test_array[1] = test2;
+    test_array[2] = test3;
+
+    evaluate_array_matroid(test_array, 3,3);
+
     //test3.function = is_pair;
-    printf("El valor de test1 es: %d\n", test2);
+    //printf("El valor de test1 es: %d\n", test2);
 
+    printf("El primer valor de I es %d \n",*(int*)(test_array[1].I));
+    //printf("El primer valor de I es %d \n",*(int*) test2.I);
 
-    evaluate_matroid(test2, 3);
+    //evaluate_matroid(test2, 3);
 }
 
     //int size_of_matroids_list = sizeof(test_array) / sizeof(test_array[0]);
